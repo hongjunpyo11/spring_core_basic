1a01a38fffd904993394c1868001815d9383a28d
hongjunpyo11 <jpyo98@naver.com>
2022-10-31 00:02:48 +0900

    회원 도메인 개발

--- src/main/java/hello/core/CoreApplication.java
+++ src/main/java/hello/core/CoreApplication.java
@@ -10,4 +10,9 @@ public class CoreApplication {
 		SpringApplication.run(CoreApplication.class, args);
 	}
 
+	/**
+	 * 참고: 프로젝트 환경설정을 편리하게 하려고 스프링 부트를 사용한 것이다.
+	 * 지금은 스프링이 없는 순수한 자바로만 개발을 진행한다는 점을 꼭 기억하자
+	 * 스프링 관련은 한참 뒤에 등장한다!!
+	 */
 }
