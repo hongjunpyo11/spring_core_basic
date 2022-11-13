0d5916fafb935079f487ca3583f2a2590d6f7dc3
hongjunpyo11 <jpyo98@naver.com>
2022-11-14 08:51:39 +0900

    인터페이스 InitializingBean, DisposableBean

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,13 +1,14 @@
 package hello.core.lifecycle;
 
-public class NetworkClient {
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient implements InitializingBean, DisposableBean {
 
     public String url;
 
     public NetworkClient() {
         System.out.println("생성자 호출, url = " + url);
-        connect();
-        call("초기화 연결 메시지");
     }
 
     public void setUrl(String url) {
@@ -27,4 +28,27 @@ public class NetworkClient {
     public void disconnect() {
         System.out.println("close " + url);
     }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("NetworkClient.afterPropertiesSet");
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("NetworkClient.destroy");
+        disconnect();
+    }
 }
+
+/**
+ * 초기화, 소멸 인터페이스 단점
+ * 이 인터페이스는 스프링 전용 인터페이스다. 해당 코드가 스프링 전용 인터페이스에 의존한다.
+ * 초기화, 소멸 메서드의 이름을 변경할 수 없다.
+ * 내가 코드를 고칠 수 업는 외부 라이브러리에 적용할 수 없다.
+ *
+ * 참고: 인터페이스를 사용하는 초기화, 종료 방법은 스프링 초창기에 나온 방법들이고,
+ * 지금은 다음의 더 나은 방법들이 있어서 거의 사용하지 않는다.
+ */
