7cc8314bc80cf293705629f6f72265a09aa3a0bb
hongjunpyo11 <jpyo98@naver.com>
2022-11-13 17:42:43 +0900

    자동, 수동 빈등록의 올바른 실무 운영 기준

--- src/test/java/hello/core/autowired/AllBeanTest.java
+++ src/test/java/hello/core/autowired/AllBeanTest.java
@@ -44,7 +44,6 @@ public class AllBeanTest {
             System.out.println("policies = " + policies);
         }
 
-
         public int discount(Member member, int price, String discountCode) {
             DiscountPolicy discountPolicy = policyMap.get(discountCode);
             return discountPolicy.discount(member, price);
@@ -56,4 +55,23 @@ public class AllBeanTest {
  * DiscountService는 Map으로 모든 DiscountPolicy 를 주입받는다. 이때 fixDiscountPolicy, rateDiscountPolicy 가 주입된다.
  * discount () 메서드는 discountCode로 "fixDiscountPolicy"가 넘어오면 map에서 fixDiscountPolicy 스프링 빈을 찾아서 실행한다.
  * 물론 “rateDiscountPolicy”가 넘어오면 rateDiscountPolicy 스프링 빈을 찾아서 실행한다.
+ */
+
+/**
+ * 자동, 수동 빈등록의 올바른 실무 운영 기준
+ * 정리
+ *   * 편리한 자동 기능을 기본으로 사용하자
+ *
+ *   * 직접 등록하는 기술 지원 객체는 수동 등록
+ *
+ *   * 다형성을 적극 활용하는 비즈니스 로직은 수동 등록을 고민해보자
+ *     * 이런 경우 설정정보만 봐도 한눈에 빈의 이름은 물론이고, 어떤 빈들이 주입될지 파악할 수 있다. 그래도 빈 자동 등록을 사용하고 싶으면
+ *       파악하기 좋게 'DiscountPolicy'의 구현 빈들만 따로 모아서 특정 패키지만 모아두자.
+ *
+ * 참고
+ * 스프링과 스프링 부트가 자동으로 등록하는 수 많은 빈들은 예외다.
+ * 이런 부분들은 스프링 자체를 잘 이해하고 스프링의 의도대로 잘 사용하는게 중요하다.
+ * 스프링 부트의 경우 DataSource 같은 데이터베이스 연결에 사용하는 기술 지원 로직까지 내부에서 자동으로 등록하는데,
+ * 이런 부분은 메뉴얼을 잘 참고해서 스프링 부트가 의도한 대로 편리하게 사용하면 된다.
+ * 반면에 스프링 부트가 아니라 내가 직접 기술 지원 객체를 스프링 빈으로 등록한다면 수동으로 등록해서 명확하게 드러내는 것이 좋다.
  */
